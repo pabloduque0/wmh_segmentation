30028f7d70742c41e0526520588818187f24dec6
Pablo Duque <menduke@live.com>
2020-11-01 19:34:45 +0100

    Updated constants file

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,25 @@
+# Preprocessing info
+
 SLICE_SHAPE = (200, 200)
-UTRECH_N_SLICES = 48
-SINGAPORE_N_SLICES = 48
-AMSTERDAM_N_SLICES = 83
 REMOVE_TOP_PRCTG = 0.05
 REMOVE_BOT_PRCTG = 0.12
 REMOVE_BOT = 6
 REMOVE_TOP = 4
+
+# Dataset folders
+
+UTRECHT_FOLDER="Utrecht"
+SINGAPORE_FOLDER="Singapore"
+AMSTERDAM_FOLDER="GE3T"
+
+# Output folders and files
+
+TRAIN_FOLDER="train"
+DATA_TRAIN_NAME="data_train.pkl"
+LABELS_TRAIN_NAME="labels_train.pkl"
+VALIDATION_FOLDER="validation"
+DATA_VALIDATION_NAME="data_validation.pkl"
+LABELS_VALIDATION_NAME="labels_validation.pkl"
+TEST_FOLDER="test"
+DATA_TEST_NAME="data_test.pkl"
+LABELS_TEST_NAME="labels_test.pkl"
