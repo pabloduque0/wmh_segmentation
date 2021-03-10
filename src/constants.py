69357df09aca75dd9a507ded8d8c71930bafd718
Pablo Duque <menduke@live.com>
2021-03-10 21:34:38 +0100

    Fixed spacing in constants

--- src/constants.py
+++ src/constants.py
@@ -8,18 +8,22 @@ REMOVE_TOP = 4
 
 # Dataset folders
 
-UTRECHT_FOLDER="Utrecht"
-SINGAPORE_FOLDER="Singapore"
-AMSTERDAM_FOLDER="GE3T"
+UTRECHT_FOLDER = "Utrecht"
+SINGAPORE_FOLDER = "Singapore"
+AMSTERDAM_FOLDER = "GE3T"
 
 # Output folders and files
 
-TRAIN_FOLDER="train"
-DATA_TRAIN_NAME="data_train.pkl"
-LABELS_TRAIN_NAME="labels_train.pkl"
-VALIDATION_FOLDER="validation"
-DATA_VALIDATION_NAME="data_validation.pkl"
-LABELS_VALIDATION_NAME="labels_validation.pkl"
-TEST_FOLDER="test"
-DATA_TEST_NAME="data_test.pkl"
-LABELS_TEST_NAME="labels_test.pkl"
+TRAIN_FOLDER = "train"
+DATA_TRAIN_NAME = "data_train.pkl"
+LABELS_TRAIN_NAME = "labels_train.pkl"
+VALIDATION_FOLDER = "validation"
+DATA_VALIDATION_NAME = "data_validation.pkl"
+LABELS_VALIDATION_NAME = "labels_validation.pkl"
+TEST_FOLDER = "test"
+DATA_TEST_NAME = "data_test.pkl"
+LABELS_TEST_NAME = "labels_test.pkl"
+EXPERIMENT_ULR_FILENAME = "url.pkl"
+ORDERED_DIR = "ordered_data"
+RAW_COPIES_DIR = "raw_copies"
+RAW_COPIES_NAME = "flair_raws.pkl"
